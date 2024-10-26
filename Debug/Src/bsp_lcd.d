47e193a69821aad5b8c2997d499a1dcb9a358de0
shinlnh <kusin0412@gmail.com>
2024-10-26 12:25:37 +0700

    BEGIN_LCDTFT_REGISTER

---
+++ Debug/Src/bsp_lcd.d
@@ -0,0 +1,9 @@
+Src/bsp_lcd.o: ../Src/bsp_lcd.c ../Inc/reg_util.h ../Inc/ili9341_reg.h \
+ ../Inc/bsp_lcd.h ../Inc/stm32f429xx.h ../Inc/reg_util.h \
+ ../Inc/ili9341_reg.h
+../Inc/reg_util.h:
+../Inc/ili9341_reg.h:
+../Inc/bsp_lcd.h:
+../Inc/stm32f429xx.h:
+../Inc/reg_util.h:
+../Inc/ili9341_reg.h:
---
+++ Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c ../Inc/bsp_lcd.h ../Inc/stm32f429xx.h \
+ ../Inc/reg_util.h ../Inc/ili9341_reg.h ../Inc/stm32f429xx.h \
+ ../Inc/reg_util.h ../Inc/ili9341_reg.h
+../Inc/bsp_lcd.h:
+../Inc/stm32f429xx.h:
+../Inc/reg_util.h:
+../Inc/ili9341_reg.h:
+../Inc/stm32f429xx.h:
+../Inc/reg_util.h:
+../Inc/ili9341_reg.h:
